a2ab060b115719f49a1b78e1589f7694c83cb947
ahmadaripintif16d <32605402+ahmadaripintif16d@users.noreply.github.com>
2017-10-08 18:40:22 +0700

    Add files via upload
    tugas algoritma dan pemograman 2 (pertemuan ke 2)

---
+++ cara memasak nasi(coding).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+cout<<"========================================="<<endl;
+cout<<"cara memasak nasi menggunakan rice cooker"<<endl;
+cout<<"========================================="<<endl;
+cout<<"1.Siapkan beras dan wadah"<<endl;
+cout<<"2.Masukan beras kedalam wadah"<<endl;
+cout<<"3.Cuci beras hinggga bersih"<<endl;
+cout<<"4.Setelah bersih masukan air sesuai ukuran"<<endl;
+cout<<"5.Masukan wadah yang berisi beras tersebut ke dalam rice cooker"<<endl;
+cout<<"6.Pastikan rice cooker dalam keadaan on"<<endl;
+cout<<"7.Lalu tekan tombol cook pada rice cooker"<<endl;
+cout<<"8.Tunggu beberapa menit sampai tombol cook berpindah ke tombol warm"<<endl;
+cout<<"9.Tunggu beberapa menit lagi dan nasi siap di hidangkan"<<endl;
+return 0;
+}
---
+++ nama (coding).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+string nama;
+cout<<"masukan nama:";
+cin>>nama;
+cout<<"nama adalah:" + nama;
+return 0;
+}
