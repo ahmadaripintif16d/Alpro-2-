944475bc98e1ae604fc7390da015f583dc83ab13
ahmadaripintif16d <32605402+ahmadaripintif16d@users.noreply.github.com>
2017-10-10 21:50:49 +0700

    Add files via upload

---
+++ prosedur.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+void Prosedur_SD();
+void Prosedur_SMP();
+void Prosedur_SMA();
+void Prosedur_Kuliah();
+
+int main()
+{
+	Prosedur_SD();
+	Prosedur_SMP();
+	Prosedur_SMA();
+	Prosedur_Kuliah();
+return 0;
+}
+
+void Prosedur_SD()
+{
+	cout << "PROSEDUR SD" << endl;
+	cout << "1. Masuk Kelas 1 SD" << endl;
+	cout << "2. Masuk Kelas 2 SD" << endl;
+	cout << "3. Masuk Kelas 3 SD" << endl;
+	cout << "4. Masuk Kelas 4 SD" << endl;
+	cout << "5. Masuk Kelas 5 SD" << endl;
+	cout << "6. Masuk Kelas 6 SD" << endl;
+}
+void Prosedur_SMP()
+{
+	cout << "PROSEDUR SMP" << endl;
+	cout << "7. Masuk Kelas 7 SMP" << endl;
+	cout << "8. Masuk Kelas 8 SMP" << endl;
+	cout << "9. Masuk Kelas 9 SMP" << endl;
+}
+void Prosedur_SMA()
+{
+	cout << "PROSEDUR SMA" << endl;
+	cout << "10. Masuk Kelas 10 SMA" << endl;
+	cout << "11. Masuk Kelas 11 SMA" << endl;
+	cout << "12. Masuk Kelas 12 SMA" << endl;
+}
+void Prosedur_Kuliah()
+{
+	cout << "PROSEDUR KULIAH" << endl;
+	cout << "13. Masuk Kuliah Semester 1" << endl;
+	cout << "14. Masuk Kuliah Semester 2" << endl;
+	cout << "15. Masuk Kuliah Semester 3" << endl;
+	cout << "16. Masuk Kuliah Semester 4" << endl;
+	cout << "17. Masuk Kuliah Semester 5" << endl;
+	cout << "18. Masuk Kuliah Semester 6" << endl;
+	cout << "19. Masuk Kuliah Semester 7" << endl;
+	cout << "20. Masuk Kuliah Semester 8" << endl;
+	cout << "21. Wisuda dan Menjadi Sarjana" << endl;
+}
---
+++ sekolah.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+cout << "=====================================" << endl;
+cout << "|  Langkah-langkah Menjadi Sarjana |" << endl;
+cout << "=====================================" << endl;
+cout << "1. Masuk Kelas 1 SD" << endl;
+cout << "2. Masuk Kelas 2 SD" << endl;
+cout << "3. Masuk Kelas 3 SD" << endl;
+cout << "4. Masuk Kelas 4 SD" << endl;
+cout << "5. Masuk Kelas 5 SD" << endl;
+cout << "6. Masuk Kelas 6 SD" << endl;
+cout << "7. Masuk Kelas 7 SMP" << endl;
+cout << "8. Masuk Kelas 8 SMP" << endl;
+cout << "9. Masuk Kelas 9 SMP" << endl;
+cout << "10. Masuk Kelas 10 SMA" << endl;
+cout << "11. Masuk Kelas 11 SMA" << endl;
+cout << "12. Masuk Kelas 12 SMA" << endl;
+cout << "13. Masuk Kuliah Semester 1" << endl;
+cout << "14. Masuk Kuliah Semester 2" << endl;
+cout << "15. Masuk Kuliah Semester 3" << endl;
+cout << "16. Masuk Kuliah Semester 4" << endl;
+cout << "17. Masuk Kuliah Semester 5" << endl;
+cout << "18. Masuk Kuliah Semester 6" << endl;
+cout << "19. Masuk Kuliah Semester 7" << endl;
+cout << "20. Masuk Kuliah Semester 8" << endl;
+cout << "21. Wisuda dan Menjadi Sarjana" << endl;
+return 0;
+}
