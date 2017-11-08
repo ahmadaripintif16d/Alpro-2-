24147e3a32dfebce58ca3a3842a3648b6ed0260e
ahmadaripintif16d <32605402+ahmadaripintif16d@users.noreply.github.com>
2017-11-08 20:10:11 +0700

    Add files via upload

---
+++ Pertemuan 6/array2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ int main () {
+ int x=3;
+ string nama1 [x];
+ cout<<"masukan nama 1:";
+ cin>>nama1 [0];
+ cout<<"masukan nama 2:";
+ cin>>nama1 [1];
+ cout<<"masukan nama 3:";
+ cin>>nama1 [2];
+
+ cout<<"==================="<<endl;
+ 
+ cout<<"nama 1 adalah:"<< nama1 [0]<<endl;
+ cout<<"nama 2 adalah:"<< nama1 [1]<<endl;
+ cout<<"nama 3 adalah:"<< nama1 [2];
+ 
+ return 0;
+ }
