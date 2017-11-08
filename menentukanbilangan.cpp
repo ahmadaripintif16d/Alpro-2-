0d324c4baa4815fec249f45e2fbae1eb063f2e12
ahmadaripintif16d <32605402+ahmadaripintif16d@users.noreply.github.com>
2017-11-08 19:28:52 +0700

    Add files via upload

---
+++ menentukanbilangan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int bil;
+cout<<"masukan bilangan:";
+cin>>bil;
+
+if(bil%2==0) {
+	cout<<"jadi bilangan ini termasuk genap"<<endl;
+}
+else{
+	cout<<"jadi blangan ini termasuk ganjil";
+}
+return 0;
+}
