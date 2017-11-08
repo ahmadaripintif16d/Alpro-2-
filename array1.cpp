daf05b77c5a086dd1ea68a0fde8a899f7027d360
ahmadaripintif16d <32605402+ahmadaripintif16d@users.noreply.github.com>
2017-11-08 19:45:54 +0700

    Add files via upload

---
+++ array1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int x;
+	string nama1 [x];
+	string nama2 [10];
+	
+	nama1 [0]="Ahmad aripin";
+	nama2 [0]="Aripin ahmad";
+	
+	cout<<nama1 [0]<<endl;
+	cout<<nama2 [0];
+	
+	return 0;
+}
+
+	
