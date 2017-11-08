3a75c66ddc332ada8423e47e47db67d45629cbfa
ahmadaripintif16d <32605402+ahmadaripintif16d@users.noreply.github.com>
2017-11-08 19:35:54 +0700

    Add files via upload

---
+++ Pertemuan 5/pengulangan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+int x;
+cout<<"masukan bilangan ganjil:";
+cin>>x;
+
+while((x%2)==0) {
+cout<<"masukan bilangan ganjil:";
+cin>>x;
+}
+return 0;
+}
+
+ 
