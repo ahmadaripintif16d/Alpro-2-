b5ef91598d57e442eacc2a19313976536637492b
ahmadaripintif16d <32605402+ahmadaripintif16d@users.noreply.github.com>
2017-11-28 22:32:44 +0700

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+string nama1,nama2,nama3,nama4,nama5,nama6,nama7,nama8,nama9,nama10;
+cout<<"masukan nama1:";
+cin>> nama1;
+cout<<"masukan nama2:";
+cin>> nama2;
+cout<<"masukan nama3:";
+cin>> nama3;
+cout<<"masukan nama4:";
+cin>> nama4;
+cout<<"masukan nama5:";
+cin>> nama5;
+cout<<"masukan nama6:";
+cin>> nama6;
+cout<<"masukan nama7:";
+cin>> nama7;
+cout<<"masukan nama8:";
+cin>> nama8;
+cout<<"masukan nama9:";
+cin>> nama9;
+cout<<"masukan nama10:";
+cin>> nama10;
+
+cout<<"==============="<<endl;
+cout<<"nama1 adalah:"<<nama1<<endl;
+cout<<"nama2 adalah:"<<nama2<<endl;
+cout<<"nama3 adalah:"<<nama3<<endl;
+cout<<"nama4 adalah:"<<nama4<<endl;
+cout<<"nama5 adalah:"<<nama5<<endl;
+cout<<"nama6 adalah:"<<nama6<<endl;
+cout<<"nama7 adalah:"<<nama7<<endl;
+cout<<"nama8 adalah:"<<nama8<<endl;
+cout<<"nama9 adalah:"<<nama9<<endl;
+cout<<"nama10 adalah:"<<nama10<<endl;
+
+return 0;
+}
