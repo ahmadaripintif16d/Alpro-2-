5ef3198bbfefb7cc7baa8083d9ac6e1917153d3f
ahmadaripintif16d <32605402+ahmadaripintif16d@users.noreply.github.com>
2017-11-28 22:46:54 +0700

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+string nama[10];
+for (int x = 0; x<10; x++){
+	cin >> nama[x];
+}
+cout<<"==================="endl;
+
+for (int x = 0; x<10; x++){
+	cout<<nama[x];
+}
+	return 0;
+}
